e36fcdb8a6453c644be220d8366af5e43ada9e24
jensi-artonest <jensikhokhariyaflutter.artonest@gmail.com>
2023-01-05 15:05:05 +0530

    05-01-2023

--- lib/screen/getclass/h_class.dart
+++ lib/screen/getclass/h_class.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 import 'package:youtubeapi/screen/getclass/homeprovider.dart';
 
  class YoutubeData extends GetxController {
-   String link =
-       "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=5&regionCode=IN&key=AIzaSyCAw6PmSZ8cYwiX5WA_hzpuDTOOe8d05EM";
+   Future<dynamic> link =
+       "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=5&regionCode=IN&key=AIzaSyCAw6PmSZ8cYwiX5WA_hzpuDTOOe8d05EM" as Future<dynamic>;
   Future<Response<Youtube>> getData(link) async => await link;
 
 }
@@ -13,8 +14,8 @@ abstract class HomeProvider extends GetConnect implements YoutubeData {
    @override
   Future<Youtube> onInit()async {
     httpClient.defaultDecoder = (val) => Youtube().fromjson(val as Map<String, dynamic>);
-    Uri uri = Uri.parse(link);
-    var res = await get("$uri");
+    Uri uri = Uri.parse(link.toString());
+    var res = await http.get(uri);
     var v1 = jsonDecode(res.body);
     print("object : ${jsonEncode(v1)}");
     return Youtube.fromJson(v1);
